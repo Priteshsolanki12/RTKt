587f3f117958d3079ab623f35a8ed5d4659ffb50
Priteshsolanki12 <solankipritesh226@gmail.com>
2025-03-07 10:51:08 +0530

    create todo

---
+++ src/features/TodoSlice.js
@@ -0,0 +1,20 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+
+const TodoSlice = createSlice({
+    name : "Todo",
+    initialState : [{id:1, text:"pratik"}],
+    reducers :{
+            addtodo:(state,action)=>{
+                state.push({id:nanoid(), text: action.payload})
+
+            },
+            removeTodo:(state,action)=>{
+                state.filter((todo)=>todo.id !== action.payLoad)
+            }
+    }
+})
+
+
+export const{} = TodoSlice.actions;
+export default TodoSlice.reducer;
